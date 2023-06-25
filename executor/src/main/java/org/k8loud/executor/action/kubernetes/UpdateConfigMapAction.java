b099523f88e9395672a5b252b7e64a0c7bd34486
Agwen12 <pawel.pycia29@gmail.com>
2023-06-25 22:16:06 +0200

    Config map update

---
+++ executor/src/main/java/org/k8loud/executor/action/kubernetes/UpdateConfigMapAction.java
@@ -0,0 +1,56 @@
+package org.k8loud.executor.action.kubernetes;
+
+import data.ExecutionExitCode;
+import data.ExecutionRS;
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.ConfigMapBuilder;
+import io.fabric8.kubernetes.client.KubernetesClient;
+
+import java.util.Map;
+
+public class UpdateConfigMapAction extends KubernetesAction {
+
+    private String namespace;
+    private String resourceName;
+
+    private Map<String, String> replacements;
+
+    public UpdateConfigMapAction(Map<String, String> params) {
+        super(params);
+    }
+
+    public UpdateConfigMapAction(Map<String, String> params, KubernetesClient client) {
+        super(params, client);
+    }
+
+    @Override
+    public void unpackParams(Map<String, String> params) {
+        namespace = params.get("namespace");
+        resourceName = params.get("resource-name");
+        // FIXME more than 1 param change pls
+        replacements = Map.of(params.get("k1"), params.get("v1"));
+    }
+
+    /**
+     * namespace:
+     * resource-name:
+     * operation-type: { replace | change-value }
+     * replacements: { key: value }
+     *
+     */
+
+    @Override
+    public ExecutionRS perform() {
+        ConfigMap cm = client.configMaps()
+                .inNamespace(namespace)
+                .withName(resourceName)
+                .edit(c -> new ConfigMapBuilder(c)
+                        .addToData(replacements).build());
+
+
+        return ExecutionRS.builder()
+                .result("Update of config map: " + resourceName + " successful")
+                .exitCode(ExecutionExitCode.OK)
+                .build();
+    }
+}
