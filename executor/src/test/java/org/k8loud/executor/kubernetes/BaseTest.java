e73ff0d8d0b1ea27be822d4f5812e62df51e4795
proman3419 <proman7268@gmail.com>
2023-10-30 00:00:34 +0100

    Missing BaseTest

---
+++ executor/src/test/java/org/k8loud/executor/kubernetes/BaseTest.java
@@ -0,0 +1,39 @@
+package org.k8loud.executor.kubernetes;
+
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.server.mock.KubernetesServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class BaseTest {
+    protected static final String NAMESPACE = "namespaceValue";
+    protected static final String RESOURCE_NAME = "resourceNameValue";
+    @Mock
+    protected KubernetesClientProvider kubernetesClientProvider;
+    protected KubernetesServer server;
+    protected KubernetesClient client;
+    protected KubernetesService kubernetesService;
+
+    @BeforeEach
+    public void setUp() {
+        server = new KubernetesServer(true, true);
+        server.before();
+        client = server.getClient();
+
+        // Set lenient strictness not to get Unnecessary Stubbing exception
+        Mockito.lenient().when(kubernetesClientProvider.getClient()).thenReturn(client);
+        kubernetesService = new KubernetesServiceImpl(kubernetesClientProvider);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (server != null) {
+            server.after();
+        }
+    }
+}
