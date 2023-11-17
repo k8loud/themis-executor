7d116adeaceb2205837689be82664cb58cda9982
proman3419 <proman7268@gmail.com>
2023-11-17 20:48:08 +0100

    Added conditional service spawning based on env variables values

--- executor/src/main/java/org/k8loud/executor/config/DroolsConfig.java
+++ executor/src/main/java/org/k8loud/executor/config/DroolsConfig.java
@@ -1,5 +1,6 @@
 package org.k8loud.executor.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.kie.api.KieServices;
 import org.kie.api.builder.KieBuilder;
 import org.kie.api.builder.KieFileSystem;
@@ -11,23 +12,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.File;
+import java.nio.file.Path;
 
+@Slf4j
 @Configuration
 public class DroolsConfig {
 
     private static final String RULES_DRL = System.getenv("RULES_PATH") != null ?
             System.getenv("RULES_PATH") :
-            "rules/test.drl";
+            Path.of("executor", "rules", "test.drl").toString();
     private static final KieServices kieServices = KieServices.Factory.get();
 
 
     @Bean
     public KieContainer kieContainer() {
-        System.out.println(RULES_DRL);
+        log.info(RULES_DRL);
         KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
         kieFileSystem.write(ResourceFactory.newFileResource(new File(RULES_DRL)));
-
-
 
         KieBuilder kb = kieServices.newKieBuilder(kieFileSystem);
         kb.buildAll();
--- executor/src/main/java/org/k8loud/executor/service/PrometheusQueryService.java
+++ executor/src/main/java/org/k8loud/executor/service/PrometheusQueryService.java
@@ -2,42 +2,10 @@ package org.k8loud.executor.service;
 
 import io.github.hephaestusmetrics.model.metrics.Metric;
 import io.github.hephaestusmetrics.model.queryresults.RawQueryResult;
-import io.github.hephaestusmetrics.serialization.Translator;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
-@Service
-public class PrometheusQueryService {
-
-    private final static String SELECTED_ENDPOINT =  "/hephaestus/metrics/selected";
-
-    private final RestTemplate restTemplate;
-    private final Translator translator = new Translator();
-
-    private static String BACKEND_URL;
-
-    public PrometheusQueryService(RestTemplateBuilder restTemplateBuilder) {
-        PrometheusQueryService.BACKEND_URL = System.getenv("HEPHAESTUS_URL");
-        this.restTemplate = restTemplateBuilder.build();
-    }
-
-    public List<Metric> queryMetrics() {
-        String url = BACKEND_URL + SELECTED_ENDPOINT;
-        RawQueryResult[] rawMetrics = restTemplate.getForObject(url, RawQueryResult[].class);
-        return Arrays.stream(Objects.requireNonNullElse(rawMetrics, new RawQueryResult[]{}))
-                .map(translator::parseResult)
-                .flatMap(result -> result.getMetrics().stream())
-                .collect(Collectors.toList());
-    }
-
-    public RawQueryResult[] queryMetricsRaw() {
-        String url = BACKEND_URL + SELECTED_ENDPOINT;
-        return restTemplate.getForObject(url, RawQueryResult[].class);
-    }
+public interface PrometheusQueryService {
+    List<Metric> queryMetrics();
+    RawQueryResult[] queryMetricsRaw();
 }
