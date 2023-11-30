41169dbf45064ccabb111112e817a501cfc0d3e7
proman3419 <proman7268@gmail.com>
2023-11-30 23:05:45 +0100

    Missing change related to prev

--- src/main/java/org/k8loud/executor/exception/code/MailExceptionCode.java
+++ src/main/java/org/k8loud/executor/exception/code/MailExceptionCode.java
@@ -1,8 +1,5 @@
 package org.k8loud.executor.exception.code;
 
 public enum MailExceptionCode {
-    CREATE_MAIL_ADDRESS_FAILURE,
-    SET_SUBJECT_FAILURE,
-    SET_CONTENT_FAILURE,
-    SEND_FAILURE
+    FAILED_TO_SEND_MAIL
 }
