7d3f45af8895456b7af2548a681b0958465c1c56
proman3419 <proman7268@gmail.com>
2023-11-30 13:30:59 +0100

    Maybe it's not needed anymore since we disable the DroolsService on local anyway?

--- src/main/java/org/k8loud/executor/drools/DroolsProperties.java
+++ src/main/java/org/k8loud/executor/drools/DroolsProperties.java
@@ -4,20 +4,9 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.nio.file.Path;
-
 @Data
 @Component
 @ConfigurationProperties(prefix = "drools")
 public class DroolsProperties {
-    private String rulesPath = getDefaultRulesPath();
-
-    private static String getDefaultRulesPath() {
-        String path = Path.of("rules", "test.drl").toString();
-        if (!(new File(path).exists())) {
-            path = Path.of("executor", path).toString();
-        }
-        return path;
-    }
+    private String rulesPath = "rules/rules.drl";
 }
