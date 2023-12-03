6e4e91829402cddc702107c78f0de1af5b6adece
Paweł Pycia <45673406+Agwen12@users.noreply.github.com>
2023-12-03 18:18:01 +0100

    add swagger and few endpoints (#124)

---
+++ src/main/java/org/k8loud/executor/controller/RulesController.java
@@ -0,0 +1,43 @@
+package org.k8loud.executor.controller;
+
+import org.apache.http.protocol.HTTP;
+import org.k8loud.executor.drools.DroolsService;
+import org.k8loud.executor.drools.RulePackage;
+import org.kie.api.definition.KiePackage;
+import org.kie.api.definition.rule.Rule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+public class RulesController {
+
+    private final DroolsService droolsService;
+
+    public RulesController(DroolsService droolsService) {
+        this.droolsService = droolsService;
+    }
+
+
+    @GetMapping(value = "/rules")
+    public ResponseEntity<List<String>> getRules() {
+        return droolsService.getRules();
+    }
+
+    @GetMapping(value = "/rules/{rulePackage}/{ruleName}")
+    public ResponseEntity<String> getRule(@PathVariable String rulePackage, @PathVariable String ruleName) {
+        return droolsService.getRule(rulePackage, ruleName);
+    }
+
+    @GetMapping(value = "/rules/{rulePackage}")
+    public ResponseEntity<String> getPackage(@PathVariable String rulePackage) {
+        return droolsService.getPackage(rulePackage);
+    }
+
+}
