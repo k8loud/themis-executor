30f9b0f8ac4c2c0495f609e9e5235cf27dca382e
proman3419 <proman7268@gmail.com>
2023-12-09 00:20:36 +0100

    Updated flag

--- src/main/java/org/k8loud/executor/actions/command/DetachNodeFromKubernetesClusterAction.java
+++ src/main/java/org/k8loud/executor/actions/command/DetachNodeFromKubernetesClusterAction.java
@@ -29,7 +29,7 @@ public class DetachNodeFromKubernetesClusterAction extends CommandAction {
     @Override
     protected String buildCommand() {
         System.out.println(nodeName);
-        return String.format("kubectl drain %1$s --ignore-daemonsets --delete-local-data && " +
+        return String.format("kubectl drain %1$s --ignore-daemonsets --delete-emptydir-data && " +
                 "kubectl delete node %1$s", nodeName);
     }
 }
