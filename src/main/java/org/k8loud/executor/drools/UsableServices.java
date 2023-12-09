4d24194229c29c9e3e4a4dbac6971fc42804fe71
proman3419 <proman7268@gmail.com>
2023-12-09 19:14:08 +0100

    Added db services to UsableServices Split db http files

--- src/main/java/org/k8loud/executor/drools/UsableServices.java
+++ src/main/java/org/k8loud/executor/drools/UsableServices.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.k8loud.executor.cnapp.sockshop.SockShopService;
 import org.k8loud.executor.command.CommandExecutionService;
+import org.k8loud.executor.db.MongoService;
+import org.k8loud.executor.db.MySQLService;
 import org.k8loud.executor.http.HTTPService;
 import org.k8loud.executor.kubernetes.KubernetesService;
 import org.k8loud.executor.mail.MailService;
@@ -18,6 +20,8 @@ public class UsableServices {
     private final CommandExecutionService commandExecutionService;
     private final KubernetesService kubernetesService;
     private final OpenstackService openstackService;
+    private final MySQLService mySQLService;
+    private final MongoService mongoService;
 
     private final HTTPService httpService;
     private final MailService mailService;
