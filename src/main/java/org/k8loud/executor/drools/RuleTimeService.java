d0ed65e9825bb3e6699e42513d9da2298aa87796
Pawel Gorgolewski <pgorgolewski01@gmail.com>
2024-06-06 11:38:45 +0200

    Adding rule timer

---
+++ src/main/java/org/k8loud/executor/drools/RuleTimeService.java
@@ -0,0 +1,27 @@
+package org.k8loud.executor.drools;
+
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.Histogram;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+@Component
+public class RuleTimeService {
+    private final Histogram histogram;
+
+    public RuleTimeService(CollectorRegistry registry) {
+        this.histogram = Histogram.build()
+                .buckets(0.5, 1, 2, 3, 5, Double.POSITIVE_INFINITY)
+                .help("rule_processing_time_metric")
+                .name("rule_processing_time_metric")
+                .labelNames("Rule")
+                .register(registry);
+    }
+
+    public void reportTime(Instant startTime, String rule) {
+        Instant endTime = Instant.now();
+        Instant ruleProcessingTime = endTime.minusSeconds(startTime.getEpochSecond());
+        histogram.labels(rule).observe(ruleProcessingTime.getEpochSecond());
+    }
+}
